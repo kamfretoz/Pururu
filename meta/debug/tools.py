3aa11e58ed1af2be205df4954fb3ead17b519ab3
KFZ <iiibrahim89@gmail.com>
2022-01-23 21:49:33 +0700

    added reload command

--- meta/debug/tools.py
+++ meta/debug/tools.py
@@ -1,4 +1,5 @@
 import os
+import hikari
 import lightbulb
 import os
 
@@ -6,18 +7,16 @@ tools_plugin = lightbulb.Plugin("toolbox")
 tools_plugin.add_checks(lightbulb.checks.owner_only)
 
 @tools_plugin.command()
-@lightbulb.add_checks(lightbulb.checks.owner_only)
 @lightbulb.command("crash", "Raise an error", aliases=["dummy", "error"])
 @lightbulb.implements(lightbulb.PrefixCommand)
-async def echo(ctx: lightbulb.Context) -> None:
+async def crash(ctx: lightbulb.Context) -> None:
     await ctx.respond("Generating an Error Message..")
     raise ValueError("This is a user generated crash")
 
 @tools_plugin.command()
-@lightbulb.add_checks(lightbulb.checks.owner_only)
 @lightbulb.command("clearterm","clear the output of the console",aliases=["clearconsole", "cc", "cls"])
 @lightbulb.implements(lightbulb.PrefixCommand)
-async def clear(self, ctx):
+async def clear(ctx: lightbulb.Context):
     """Clear the console."""
     if os.name == "nt":
         os.system("cls")
@@ -28,6 +27,24 @@ async def clear(self, ctx):
             for _ in range(100):
                 print()
     await ctx.respond("Console cleared successfully.")
+    
+@tools_plugin.command()
+@lightbulb.command("extension", "manage an extension")
+@lightbulb.implements(lightbulb.PrefixCommandGroup, lightbulb.SlashCommandGroup)
+async def extension_manager(ctx:lightbulb.Context):
+    pass
+
+@extension_manager.child()
+@lightbulb.option("name", "the extension you want to reload", hikari.OptionType.STRING, required=True)
+@lightbulb.option("category", "the category of the extension", required = True, choices=["fun", "information", "moderation", "utilities"])
+@lightbulb.command("reload", "Reload an extension")
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def extension_reload(ctx:lightbulb.Context):
+    name = ctx.options.name
+    category = ctx.options.category
+    await ctx.respond(f"Reloading the extension `{ctx.options.name}`")
+    ctx.bot.reload_extensions(f"extensions.{category}.{name}")
+    await ctx.edit_last_response(f"Successfully reloaded `{ctx.options.name}`!")
 
 
 def load(bot):
