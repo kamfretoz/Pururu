29c96e75283e4d787fa8feb890d0951f5a2ec068
KFZ <iiibrahim89@gmail.com>
2022-01-25 19:25:27 +0700

    Bunch of improvement and fixes to anime command

--- extensions/utilities/myanimelist.py
+++ extensions/utilities/myanimelist.py
@@ -1,3 +1,6 @@
+from email.policy import default
+from pydoc import synopsis
+from random import choice, choices
 import lightbulb
 import hikari
 import json
@@ -9,39 +12,49 @@ from textwrap import shorten
 mal_plugin = lightbulb.Plugin("myanimelist")
 
 @mal_plugin.command()
+@lightbulb.command("myanimelist", "Allows you to look up anime informations!", aliases=["mal"])
+@lightbulb.implements(lightbulb.PrefixCommandGroup, lightbulb.SlashCommandGroup)
+async def myanimelist(ctx: lightbulb.Context) -> None:
+    pass
+
+@myanimelist.child
 @lightbulb.option("name", "The anime you want to lookup", hikari.OptionType.STRING, required=True)
-@lightbulb.command("anime", "Find the information of an Anime", aliases=["mal"], auto_defer = True)
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
-async def myanime(ctx: lightbulb.Context) -> None:
+@lightbulb.command("anime", "Find the information of an Anime", aliases=["ani"], auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def myanimelist_anime(ctx: lightbulb.Context) -> None:
     name = ctx.options.name
+    
     parameters = {
         "q": name,
-        "limit": 1
+        "limit": 1,
+        "page": 1,
+        "sort": "asc"
     }
     try:
-        async with ctx.bot.d.aio_session.get('https://api.jikan.moe/v3/search/anime', params=parameters, timeout=10) as resp:
+        async with ctx.bot.d.aio_session.get('https://api.jikan.moe/v4/anime', params=parameters, timeout=10) as resp:
             data = json.loads(await resp.read())
     except asyncio.exceptions.TimeoutError:
         raise ValueError("⚠ A Timeout Error occured, please try again!")
-    try:
-        anime_id = data["results"][0]["mal_id"]
-        anime_title = data["results"][0]["title"]
-        anime_url = data["results"][0]["url"]
-        anime_img = data["results"][0]["image_url"]
-        anime_status = data["results"][0]["airing"]
-        anime_synopsis = data["results"][0]["synopsis"]
-        anime_type = data["results"][0]["type"]
-        score = data["results"][0]["score"]
-    except KeyError:
-        raise ValueError("⚠ Result not found, maybe check your input?")
+    
+    if not data["data"]:
+        raise ValueError("⚠ No result found.")
+
+    anime_id = data["data"][0]["mal_id"]
+    anime_title = data["data"][0]["title"]
+    anime_url = data["data"][0]["url"]
+    anime_img = data["data"][0]["images"]["webp"]["large_image_url"]
+    anime_status = data["data"][0]["status"]
+    anime_synopsis = data["data"][0]["synopsis"]
+    anime_type = data["data"][0]["type"]
+    score = data["data"][0]["score"]
 
     emb = hikari.Embed(
         title="MyAnimeList Anime Information", timestamp=datetime.now().astimezone())
     if score == None or score == 0:
         score = "N/A"
-    start = data["results"][0]["start_date"]
-    end = data["results"][0]["end_date"]
-    mem = data["results"][0]["members"]
+    start = data["data"][0]["aired"]["from"]
+    end = data["data"][0]["aired"]["to"]
+    mem = data["data"][0]["members"]
     # Time zone converter (a few checks will depends on the presence of time_end value)
     try:
         time_start = ciso8601.parse_datetime(start)
@@ -54,26 +67,20 @@ async def myanime(ctx: lightbulb.Context) -> None:
     except TypeError:
         formatted_end = "Unknown"
     try:
-        total_episode = data["results"][0]["episodes"]
+        total_episode = data["data"][0]["episodes"]
         if total_episode == 0 or total_episode is None:
             total_episode = "Not yet determined"
     except TypeError:
         total_episode = "Not yet determined."
-    if anime_status:
-        anime_status = "Ongoing"
-    elif not anime_status:
-        if start is None:
-            anime_status = "Not yet aired"
-        else:
-            anime_status = "Finished airing"
-    if len(anime_synopsis) > 1024:
-        shorten(anime_synopsis,width=1020,placeholder="...")
     emb.set_image(anime_img)
     emb.set_thumbnail("https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png")
-    emb.set_footer("Powered by: Jikan")
+    emb.set_footer("Powered by: Jikan", icon="https://jikan.moe/assets/images/logo/jikan.logo.png")
     emb.add_field(name="📝 Title",value=f"[{anime_title}]({anime_url})", inline=False)
     if anime_synopsis:
-        emb.add_field(name="ℹ Synopsis",value=anime_synopsis, inline=False)
+        synopsis = anime_synopsis
+        if len(anime_synopsis) > 1024:
+            synopsis = shorten(anime_synopsis,width=1000,placeholder="...")
+        emb.add_field(name="ℹ Synopsis",value=synopsis, inline=False)
     else:
         emb.add_field(name="ℹ Synopsis",value="No Synopsis Found.", inline=False)
     emb.add_field(name="⌛ Status", value=anime_status, inline=False)
@@ -83,27 +90,117 @@ async def myanime(ctx: lightbulb.Context) -> None:
     emb.add_field(name="💿 Episodes", value=total_episode, inline=True)
     emb.add_field(name="⭐ Score", value=f"{score}", inline=True)
     try:
-        rate = data["results"][0]["rated"]
-        if rate is None:
+        rating = data["data"][0]["rating"]
+        if rating is None:
             rating = "Unknown"
-        else:
-            rating = {
-                'G': 'All Ages (G)',
-                'PG': 'Children (PG)',
-                'PG-13': 'Teens 13 or Older (PG-13)',
-                'R': '17+ Recommended, (Violence & Profanity) (R)',
-                'R+': 'Mild Nudity, (May also contain Violence & Profanity) (R+)',
-                'Rx': 'Hentai, (Extreme sexual content/Nudity) (Rx)'
-            }.get(str(rate))
         emb.add_field(name="🔞 Rating", value=rating, inline=True)
     except IndexError:
         pass
-    except AttributeError:
-        pass
-    except KeyError:
-        pass
     emb.add_field(name="👥 Members", value=mem, inline=True)
     emb.add_field(name="💳 ID", value=anime_id, inline=True)
+    await ctx.respond(embed=emb)
+    
+@myanimelist.child
+@lightbulb.option("name", "The manga you want to lookup", hikari.OptionType.STRING, required=True)
+@lightbulb.command("manga", "Find the information of an manga", aliases=["man"], auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def myanimelist_manga(ctx: lightbulb.Context):
+    name = ctx.options.name
+    parameters = {
+        "q": name,
+        "limit": 1,
+        "page": 1,
+        "sort": "asc"
+    }
+    try:
+        async with ctx.bot.d.aio_session.get(f'https://api.jikan.moe/v4/manga', params=parameters, timeout=10) as resp:
+            data = json.loads(await resp.read())
+    except asyncio.exceptions.TimeoutError:
+        raise ValueError("⚠ A Timeout Error occured, please try again!")
+    
+    if not data["data"]:
+        raise ValueError("⚠ No result found.")
+    
+    manga_title = data["data"][0]["title"]
+    manga_url = data["data"][0]["url"]
+    img_url = data["data"][0]["images"]["webp"]["large_image_url"]
+    stat = data["data"][0]["status"]
+    manga_synopsis = data["data"][0]["synopsis"]
+    manga_type = data["data"][0]["type"]
+    manga_chapters = data["data"][0]["chapters"]
+    manga_volumes = data["data"][0]["volumes"]
+    score = data["data"][0]["scored"]
+    pub_date = data["data"][0]["published"]["from"]
+    memb = data["data"][0]["members"]
+    manga_id = data["data"][0]["mal_id"]
+    time_start = ciso8601.parse_datetime(pub_date)
+    formatted_start = time_start.strftime("%B %d, %Y")
+    
+    if manga_volumes is None or manga_volumes == 0:
+        manga_volumes = "Unknown"
+    if manga_chapters is None or manga_chapters == 0:
+        manga_chapters = "Unknown"
+    emb = hikari.Embed(
+        title="MyAnimeList Manga Information", timestamp=datetime.now().astimezone())
+    emb.set_image(img_url)
+    emb.set_thumbnail("https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png")
+    emb.set_footer("Powered by: Jikan", icon="https://jikan.moe/assets/images/logo/jikan.logo.png")
+    emb.add_field(name="📑 Title",value=f"[{manga_title}]({manga_url})", inline=False)
+    if manga_synopsis:
+        synopsis = manga_synopsis
+        if len(manga_synopsis) > 1024:
+            synopsis = shorten(manga_synopsis,width=1000,placeholder="...")
+        emb.add_field(name="ℹ Synopsis",value=synopsis, inline=False)
+    else:
+        emb.add_field(name="ℹ Synopsis",value="No Synopsis Found.", inline=False)
+    emb.add_field(name="⏳ Status", value=stat, inline=False)
+    emb.add_field(name="📁 Type", value=manga_type, inline=False)
+    emb.add_field(name="📅 Publish Date",value=formatted_start, inline=False)
+    emb.add_field(name="📚 Volumes", value=manga_volumes, inline=True)
+    emb.add_field(name="📰 Chapters", value=manga_chapters, inline=True)
+    emb.add_field(name="⭐ Score", value=f"{score}", inline=True)
+    emb.add_field(name="👥 Members", value=memb, inline=True)
+    emb.add_field(name="💳 ID", value=manga_id, inline=True)
+    await ctx.respond(embed=emb)
+
+@myanimelist.child
+@lightbulb.option("name", "The character name you want to lookup", hikari.OptionType.STRING, required=True)
+@lightbulb.command("character", "Find the information of an anime character", aliases=["chara"], auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def myanimelist_chara(ctx: lightbulb.Context):
+    name = ctx.options.name
+    parameters = {
+        "q": name,
+        "limit": 1
+    }
+    try:
+        async with ctx.bot.d.aio_session.get(f'https://api.jikan.moe/v4/characters', params=parameters, timeout=10) as resp:
+            data = json.loads(await resp.read())
+    except asyncio.exceptions.TimeoutError:
+        raise ValueError("⚠ A Timeout Error occured, please try again!")
+    if not data["data"]:
+        raise ValueError("⚠ No result found.")
+    char_id = data["data"][0]["mal_id"]
+    char_url = data["data"][0]["url"]
+    char_img = data["data"][0]["images"]["webp"]["image_url"]
+    char_name = data["data"][0]["name"]
+    char_about = data["data"][0]["about"]
+    
+    emb = hikari.Embed(title="MyAnimeList Character Information", timestamp=datetime.now().astimezone())
+    emb.set_image(char_img)
+    emb.set_thumbnail("https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png")
+    emb.set_footer("Powered by: Jikan", icon="https://jikan.moe/assets/images/logo/jikan.logo.png")
+    emb.add_field(name="👤 Name", value=f"[{char_name}]({char_url})", inline=False)
+    try:
+        char_nick = data["data"][0]["nicknames"][0]
+        emb.add_field(name="👤 Nickname", value=f"{char_nick}", inline=False)
+    except IndexError:
+        pass
+    about = char_about
+    if len(about) > 1024:
+        about = shorten(char_about, width=1000,placeholder="...")
+    emb.add_field(name="ℹ️ About", value=about, inline=False)
+    emb.add_field(name="💳 ID", value=char_id, inline=True)
     await ctx.respond(embed=emb)
 
 def load(bot):
