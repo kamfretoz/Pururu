3b268647415333bf38d46bd4edef49077256d684
KFZ <iiibrahim89@gmail.com>
2022-01-26 17:33:07 +0700

    add amiibo command

---
+++ extensions/utilities/amiibo.py
@@ -0,0 +1,50 @@
+import lightbulb
+import hikari
+
+amiibo_plugin = lightbulb.Plugin("amiibo", "For Nintendo's Amiibo related command")
+
+@amiibo_plugin.command()
+@lightbulb.option("query", "The amiibo you want to look up", hikari.OptionType.STRING, required=True)
+@lightbulb.command("amiibo", "Look up information on Nintendo's Amiibo", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def amiibo(ctx: lightbulb.Context) -> None:
+    #--First we connect to the Amiibo API and download the Amiibo information--#
+    query = ctx.options.query
+    parameters = {
+        "name": query
+    }
+    async with ctx.bot.d.aio_session.get('https://amiiboapi.com/api/amiibo/', params=parameters) as amiibo:
+        data = await amiibo.json()
+        #--Now we attempt to extract information--#
+        try:
+            series = data['amiibo'][0]['amiiboSeries']
+            character = data['amiibo'][0]['character']
+            name = data['amiibo'][0]['name']
+            game = data['amiibo'][0]['gameSeries']
+            atype = data['amiibo'][0]['type']
+            na_release = data['amiibo'][0]['release']['na']
+            eu_release = data['amiibo'][0]['release']['eu']
+            jp_release = data['amiibo'][0]['release']['jp']
+            au_release = data['amiibo'][0]['release']['au']
+            image = data['amiibo'][0]['image']
+            #--Finally, we format it into a nice little embed--#
+            embed = hikari.Embed(
+                title=f"Amiibo information for {name} ({series} series)", color=0xd82626)
+            embed.add_field(name='Character Represented', value=character)
+            embed.add_field(name='Amiibo Series', value=f"{series} series")
+            embed.add_field(name='Game of Origin', value=game)
+            embed.add_field(name='Type', value=atype)
+            embed.add_field(
+                name='Released', value=f":flag_us: {na_release}\n:flag_eu: {eu_release}\n:flag_jp: {jp_release}\n:flag_au: {au_release}")
+            embed.set_image(image)
+            embed.set_thumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Amiibo.svg/1024px-Amiibo.svg.png")
+            await ctx.respond(embed=embed)
+        except KeyError:
+            raise ValueError("❌ I couldn't find any Amiibo with that name. Double-check your spelling and try again.")
+
+
+def load(bot):
+    bot.add_plugin(amiibo_plugin)
+
+def unload(bot):
+    bot.remove_plugin(amiibo_plugin)
