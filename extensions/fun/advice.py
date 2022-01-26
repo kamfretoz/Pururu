0c8dd3717cfd57a1537524aaf8fa5ea4ee9ab60d
KFZ <iiibrahim89@gmail.com>
2022-01-26 20:02:54 +0700

    quotes command

---
+++ extensions/fun/advice.py
@@ -0,0 +1,22 @@
+import lightbulb
+import hikari
+import json
+
+advice_plugin = lightbulb.Plugin("advices", "Some advices that might aid you in your journey of life! :)")
+
+@advice_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.command("advice", "Send useful advices!.", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def dad(ctx: lightbulb.Context) -> None:
+    async with ctx.bot.d.aio_session.get(f'https://api.adviceslip.com/advice') as resp:
+        data = json.loads(await resp.read())
+    adv = data["slip"]["advice"]
+    emb = hikari.Embed(title="Here's some advice for you :)", description=adv)
+    await ctx.respond(embed=emb)
+
+def load(bot):
+    bot.add_plugin(advice_plugin)
+
+def unload(bot):
+    bot.remove_plugin(advice_plugin)
---
+++ extensions/fun/quotes.py
@@ -0,0 +1,23 @@
+import lightbulb
+import hikari
+
+quote_plugin = lightbulb.Plugin("quote", "Some quotes that you might find useful :)")
+
+@quote_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.command("quote", "Send quotes!.", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def dad(ctx: lightbulb.Context) -> None:
+    async with ctx.bot.d.aio_session.get('https://quote-garden.herokuapp.com/api/v3/quotes/random') as resp:
+        data = await resp.json()
+    quote = data["data"][0]["quoteText"]
+    author = data["data"][0]["quoteAuthor"]
+    emb = hikari.Embed(description=f"**{quote}**")
+    emb.set_footer(f"Quote by: {author}")
+    await ctx.respond(embed=emb)
+
+def load(bot):
+    bot.add_plugin(quote_plugin)
+
+def unload(bot):
+    bot.remove_plugin(quote_plugin)
