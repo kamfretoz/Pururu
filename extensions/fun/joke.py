fee4e49de174f7112440a412ba153cf373e5cc62
KFZ <iiibrahim89@gmail.com>
2022-01-26 19:26:48 +0700

    added joke command

---
+++ extensions/fun/joke.py
@@ -0,0 +1,44 @@
+import lightbulb
+import hikari
+
+joke_plugin = lightbulb.Plugin("joke", "Jokes! But be wary for the offensive ones!")
+
+@joke_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("lang", "The language of the joke", str, required=False, default = "en", choices = ["cs","de","en","es","fr","pt"])
+@lightbulb.command("joke", "For all kinds of jokes! (Some might be offensive, be careful.)")
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def joke(ctx: lightbulb.Context) -> None:
+    parameters = {
+        "format": "json",
+        "amount": 1,
+        "lang": ctx.options.lang
+    }
+    
+    async with ctx.bot.d.aio_session.get(f'https://v2.jokeapi.dev/joke/Any', params = parameters) as resp:
+        resp.raise_for_status()
+        data = await resp.json()
+        
+    emb = hikari.Embed(title="Here comes a joke!")
+        
+    jokecategory = data["category"]
+    thetype = data["type"]
+    
+    if thetype == "twopart":
+        setup = data["setup"]
+        delivery = data["delivery"]
+        emb.add_field(name=f"Category: **{jokecategory}**", value=f"{setup}\n{delivery}")
+    if thetype == "single":
+        joke = data["joke"]
+        emb.add_field(name=f"Category: **{jokecategory}**", value=joke)
+    if data["error"] == "true":
+        return await ctx.reply("An Error has occured!")
+        
+    await ctx.respond(embed=emb)
+    
+
+def load(bot):
+    bot.add_plugin(joke_plugin)
+
+def unload(bot):
+    bot.remove_plugin(joke_plugin)
