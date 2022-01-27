3917b46fa6f7a9d4b80f9043bf690554b726cc60
KFZ <iiibrahim89@gmail.com>
2022-01-27 19:36:43 +0700

    moar image command

--- extensions/fun/images.py
+++ extensions/fun/images.py
@@ -1,5 +1,6 @@
 import lightbulb
 import hikari
+import io
 
 
 img_plugin = lightbulb.Plugin("images", "Images manipulation related command")
@@ -120,7 +121,261 @@ async def pats(ctx: lightbulb.Context):
             description=f"{ctx.author.mention} gives {user.mention} some headpats!", color=0x8253c3)
         embed.set_image(result)
         await ctx.respond(embed=embed)
+        
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the horny one!", hikari.User , required = True)
+@lightbulb.command("horny", "Horny card for u", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def horny(ctx: lightbulb.Context):
+    mem = ctx.options.user
+    parameters = {
+        "avatar" : str(mem.avatar_url)
+    }
     
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/horny', params = parameters) as af:
+        if 300 > af.status >= 200:
+            fp = io.BytesIO(await af.read())
+            fp.seek(0)
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(fp)
+            await ctx.respond(embed=em)
+        else:
+            await ctx.respond('No horny :(')
+            
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("text", "the text you want to write!", str , required = True)
+@lightbulb.option("user", "the name of the user!", hikari.User , required = True)
+@lightbulb.command("tweet", "create a fake tweet", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def tweet(ctx:lightbulb.Context):
+    member = ctx.options.user
+    message = ctx.options.text
+    parameters = {
+        "avatar" : str(member.avatar_url),
+        "username" : member.username,
+        "displayname" : member.display_name or member.username,
+        "comment" : message
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/tweet', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            imageData.seek(0)
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            await ctx.respond(attachment=em) # sending the file
+    
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("text", "the text you want to write!", str , required = True)
+@lightbulb.option("user", "the name of the user!", hikari.User , required = True)
+@lightbulb.command("ytcomment", "create a youtube comment", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def yt(ctx:lightbulb.Context):
+    member = ctx.options.user
+    message = ctx.options.text
+    parameters = {
+        "avatar" : str(member.avatar_url),
+        "username" : member.username,
+        "comment" : message
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/youtube-comment', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            imageData.seek(0)
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            await ctx.respond(embed=em) # sending the file
+            
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("comrade", "☭", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def comrade(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/comrade', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+            
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("gay", "the gay-laser", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def gay(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/gay', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("text", "the text you want to write!", str , required = True)
+@lightbulb.option("user", "the name of the user!", hikari.User , required = True)
+@lightbulb.command("stupid", "Oh no its stupid", auto_defer = True, aliases = ["sputid"])
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def stupid(ctx:lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url),
+        "dog" : ctx.options.text
+    }
+
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/its-so-stupid', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            await ctx.respond(embed=em) # sending the file
+
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("lolipolice", "the police coming to your house", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def lolipolice(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/lolice', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+            
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("simpcard", "this is certified simp moment", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def simpcard(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/simpcard', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    title=f"what a simp, {member.username}.",
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("jail", "Welcome to the Jail", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def jail(ctx: lightbulb.Context):
+    member = ctx.options.user
+
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/jail', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    title=f"{member.username} have been jailed.",
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("kill", "You Died", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def wasted(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/wasted', params=parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    title=f"{member.username} has died",
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("missionpass", "Mission Passed! Respect++", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def passed(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/passed', params = parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    title=f"Mission passed",
+                    description="Respect +100",
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file
+            
+@img_plugin.command()
+@lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
+@lightbulb.option("user", "the target user!", hikari.User , required = True)
+@lightbulb.command("triggered", "TRIGGERED", auto_defer = True)
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def triggered(ctx: lightbulb.Context):
+    member = ctx.options.user
+    parameters = {
+        "avatar" : str(member.avatar_url)
+    }
+    async with ctx.bot.d.aio_session.get(f'https://some-random-api.ml/canvas/triggered', params = parameters) as resp:
+            imageData = io.BytesIO(await resp.read()) # read the image/bytes
+            em = hikari.Embed(
+                    title=f"{member.name} have been triggered!",
+                    color=0xf1f1f1,
+                )
+            em.set_image(imageData)
+            
+            await ctx.respond(embed=em) # sending the file          
+
 def load(bot):
     bot.add_plugin(img_plugin)
 
