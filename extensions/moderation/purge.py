1d1b1e63070f62533f23d674c979f2f1d06a296a
kamfretoz <iiibrahim89@gmail.com>
2022-02-06 22:44:02 +0700

    small improvement to purge

--- extensions/moderation/purge.py
+++ extensions/moderation/purge.py
@@ -12,7 +12,7 @@ purge_plugin = lightbulb.Plugin("purge", "Burn down the evidence! *evil laugh*")
     lightbulb.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES)
 )
 @lightbulb.option("amount", "The number of messages to purge.", type=int, required=True, max_value = 500)
-@lightbulb.command("purge", "Purge messages from this channel.", aliases=["clear"])
+@lightbulb.command("purge", "Purge messages from this channel.", aliases=["clear","prune"])
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 @filament.utils.pass_options
 async def purge_messages(ctx: lightbulb.Context, amount) -> None:
@@ -26,10 +26,7 @@ async def purge_messages(ctx: lightbulb.Context, amount) -> None:
     msgs = await ctx.bot.rest.fetch_messages(channel).limit(amount)
     await ctx.bot.rest.delete_messages(channel, msgs)
 
-    resp = await ctx.respond(f"{len(msgs)} messages deleted")
-
-    await asyncio.sleep(5)
-    await resp.delete()
+    await ctx.respond(f"**{len(msgs)} messages deleted**", delete_after=5)
 
 
 def load(bot: lightbulb.BotApp) -> None:
