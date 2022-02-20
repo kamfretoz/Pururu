1e76ad6c98b88d191cfff0ce71df7103a3484158
kamfretoz <iiibrahim89@gmail.com>
2022-02-20 23:21:33 +0700

    lowercase kym plugin name

--- extensions/fun/knowyourmeme.py
+++ extensions/fun/knowyourmeme.py
@@ -3,7 +3,7 @@ import hikari
 from lightbulb.ext import filament
 from bs4 import BeautifulSoup
 
-kym_plugin = lightbulb.Plugin("KnowYourMeme", "Find information about a meme!")
+kym_plugin = lightbulb.Plugin("knowyourmeme", "Find information about a meme!")
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
