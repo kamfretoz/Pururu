829cf5c46007444b6edaa21adf773d150793eca1
kamfretoz <iiibrahim89@gmail.com>
2022-02-20 20:55:55 +0700

    Remove echo from slash

--- extensions/utilities/echo.py
+++ extensions/utilities/echo.py
@@ -6,15 +6,15 @@ echo_plugin = lightbulb.Plugin("echo", "Is anyone there?")
 @echo_plugin.command()
 @lightbulb.option("text", "The text you want to repeat", str, required=True, modifier = lightbulb.commands.OptionModifier.CONSUME_REST)
 @lightbulb.command("echo", "Repeats the text that you have given", aliases=["say"])
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.PrefixCommand)
 @filament.utils.pass_options
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.PrefixCommand)
 async def echo(ctx: lightbulb.Context, text) -> None:
     await ctx.respond(text)
     
 @echo_plugin.command()
 @lightbulb.command("sua", "irma")
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.PrefixCommand)
 async def sua(ctx: lightbulb.Context) -> None:
     await ctx.respond("irma")
 
