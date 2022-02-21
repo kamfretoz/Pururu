c188ce2dc210cefa7e3b7bae6396c15fbb00cba4
kamfretoz <iiibrahim89@gmail.com>
2022-02-21 12:53:54 +0700

    Remove useless check on color command

--- extensions/utilities/color.py
+++ extensions/utilities/color.py
@@ -1,5 +1,7 @@
+from pickletools import optimize
 import lightbulb
 import io
+import hikari
 from re import match
 from PIL import Image
 from lightbulb.ext import filament
@@ -8,26 +10,21 @@ hex_color_plugin = lightbulb.Plugin("color", "#RGBforLyfe")
 
 @hex_color_plugin.command()
 @lightbulb.add_cooldown(3, 1, lightbulb.cooldowns.UserBucket)
-@lightbulb.option("colour_codes", "HEX Code for the color", type = str, required = True, modifier = lightbulb.commands.OptionModifier.CONSUME_REST)
+@lightbulb.option("colour_code", "HEX Code for the color", hikari.Color, required = True, modifier = lightbulb.commands.OptionModifier.CONSUME_REST)
 @lightbulb.command("color", "Posts color of given hex code", aliases=["getcolor", "colour", "getcolour"])
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 @filament.utils.pass_options
-async def color(ctx: lightbulb.Context, colour_codes):
-    colour_codes = colour_codes.split()
-    size = (60, 80) if len(colour_codes) > 1 else (200, 200)
-    for colour_code in colour_codes:
-        hex_regex = '^#?(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$'
-        hex_match = match(hex_regex, colour_code)
-        if hex_match:
-            if not colour_code.startswith("#"):
-                colour_code = "#" + colour_code
-            image = Image.new("RGB", size, colour_code)
-            with io.BytesIO() as file:
-                image.save(file, "PNG")
-                file.seek(0)
-                await ctx.respond(f"Colour with hex code: `{colour_code}`", attachment=file)
-        else:
-            await ctx.respond(f"Invalid Color Code: `{colour_codes}`")
+async def color(ctx: lightbulb.Context, colour_code):
+    hex_regex = '^#?(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$'
+    hex_match = match(hex_regex, str(colour_code))
+    if hex_match:
+        image = Image.new("RGB", (256, 256), colour_code)
+        with io.BytesIO() as file:
+            image.save(file, "PNG", optimize=True)
+            file.seek(0)
+            await ctx.respond(f"Colour with hex code: `{colour_code}`", attachment=file)
+    else:
+        await ctx.respond(f"Invalid Color Code: `{colour_code}`")
 def load(bot) -> None:
     bot.add_plugin(hex_color_plugin)
     
