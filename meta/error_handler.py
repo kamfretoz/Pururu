616cbcb7028aa6c3bebfef46ba9037ce141203b9
kamfretoz <iiibrahim89@gmail.com>
2022-02-25 16:27:56 +0700

    use logging and move embed func out of the command

--- meta/error_handler.py
+++ meta/error_handler.py
@@ -1,3 +1,4 @@
+import logging
 import lightbulb
 import hikari
 import random
@@ -18,48 +19,46 @@ error_message = {
             "ForbiddenError": "I'm not allowed to do that!"
         }
 
+async def send_embed(name, code, event, *args):
+    message = error_message[name]
+    if args:
+        message = message.format(*args)
+    err = hikari.Embed(description=f"**:warning: {message}**", timestamp=datetime.datetime.now().astimezone(), color=0xFF0000)
+    if code:
+        err.set_image(f"https://http.cat/{code}.jpg")
+    await event.context.respond(content=f"{random.choice(error_quotes)}", embed=err)
+
 async def on_error(event: lightbulb.CommandErrorEvent) -> None:
     if isinstance(event.exception, lightbulb.CommandInvocationError):
-        print(f"Ignoring exception in command {event.context.command.name}")
-        
         errormsg = hikari.Embed(title=f"🛑 An error occurred with the `{event.context.command.name}` command.", color=0xFF0000, timestamp=datetime.datetime.now().astimezone())
         errormsg.set_image("https://http.cat/500.jpg")
         errormsg.add_field(name="📜 **__Error Log__**:", value=f"```py\n{event.exception.__cause__}```")
         await event.context.respond(embed=errormsg, content=random.choice(error_quotes))
-        raise event.exception
+        logging.error(event.exception)
 
     # Unwrap the exception to get the original cause
     exception = event.exception.__cause__ or event.exception
-
-    async def send_embed(name, code: int, *args):
-        message = error_message[name]
-        if args:
-            message = message.format(*args)
-        err = hikari.Embed(description=f"**:warning: {message}**", timestamp=datetime.datetime.now().astimezone(), color=0xFF0000)
-        if code:
-            err.set_image(f"https://http.cat/{code}.jpg")
-        await event.context.respond(content=f"{random.choice(error_quotes)}", embed=err)
     
     if isinstance(exception, lightbulb.errors.CommandNotFound):
-        await send_embed("CommandNotFound", 404 ,exception.invoked_with)
+        await send_embed("CommandNotFound", 404, event, exception.invoked_with)
     elif isinstance(exception, lightbulb.errors.MissingRequiredPermission):
-        await send_embed("MissingPermissions", 403 ,exception.missing_perms)
+        await send_embed("MissingPermissions", 403, event, exception.missing_perms)
     elif isinstance(exception, lightbulb.errors.NotEnoughArguments):
-        await send_embed("MissingRequiredArgument", 410 ,exception.missing_options)
+        await send_embed("MissingRequiredArgument", 410, event, exception.missing_options)
     elif isinstance(exception, lightbulb.CommandIsOnCooldown):
-        await send_embed("CommandOnCooldown", 429 ,int(exception.retry_after))
+        await send_embed("CommandOnCooldown", 429, event, int(exception.retry_after))
     elif isinstance(exception, lightbulb.errors.BotMissingRequiredPermission):
-        await send_embed("BotMissingPermissions", 403 ,exception.missing_perms)
+        await send_embed("BotMissingPermissions", 403, event, exception.missing_perms)
     elif isinstance(exception, lightbulb.errors.NotOwner):
-        await send_embed("NotOwner", 401)
+        await send_embed("NotOwner", 401, event)
     elif isinstance(exception, lightbulb.errors.ConverterFailure):
-        await send_embed("ConversionError", 400 ,exception.option)
+        await send_embed("ConversionError", 400, event, exception.option)
     elif isinstance(exception, lightbulb.OnlyInGuild):
-        await send_embed("NoPrivateMessage", 423)
+        await send_embed("NoPrivateMessage", 423, event)
     elif isinstance(exception, lightbulb.errors.NSFWChannelOnly):
-        await send_embed("NSFWChannelRequired", 423)
+        await send_embed("NSFWChannelRequired", 423, event)
     elif isinstance(exception, lightbulb.errors.CheckFailure):
-        await send_embed("CheckFailure", 401)
+        await send_embed("CheckFailure", 401, event)
     
 def load(bot):
     bot.subscribe(lightbulb.CommandErrorEvent, on_error)
