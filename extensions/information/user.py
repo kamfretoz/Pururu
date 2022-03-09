bd1ee0dc31c0b9a2dd833a63fca7fc761221e7cc
kamfretoz <iiibrahim89@gmail.com>
2022-03-09 14:47:53 +0700

    fix target option requirement for banner command

--- extensions/information/user.py
+++ extensions/information/user.py
@@ -93,7 +93,7 @@ async def  user_info(ctx: lightbulb.Context, target) -> None:
 
 @user_plugin.command
 @lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
-@lightbulb.option("target", "The member to get the banner.", hikari.User, required=True)
+@lightbulb.option("target", "The member to get the banner.", hikari.User, required=False)
 @lightbulb.command("banner", "Get a member's banner.", auto_defer = True, ephemeral = True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 @filament.utils.pass_options
