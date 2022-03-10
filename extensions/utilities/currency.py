e570cb29ba2fedab509e2b98053ceda8f8aed965
kamfretoz <iiibrahim89@gmail.com>
2022-03-10 21:24:56 +0700

    Replace Currency Command's API with another one

--- extensions/utilities/currency.py
+++ extensions/utilities/currency.py
@@ -1,3 +1,4 @@
+from ctypes.wintypes import INT
 import lightbulb
 import hikari
 import json
@@ -8,41 +9,44 @@ from lightbulb.ext import filament
 
 curr_plugin = lightbulb.Plugin("currency", "money money money")
 
-# To retrieve KSoft.Si API KEY
 dotenv.load_dotenv()
-ksoft_key = os.environ.get("KSOFT_API_KEY")
+curr_key = os.environ["GEO_CURR_API"]
 
 @curr_plugin.command()
 @lightbulb.add_cooldown(3, 3, lightbulb.cooldowns.UserBucket)
-@lightbulb.option("value", "The amount you want to convert", int, required=True)
+@lightbulb.option("val", "The amount you want to convert", int, required=True)
 @lightbulb.option("to", "The currency you want to convert to", str, required=True)
 @lightbulb.option("origin", "The currency you want to convert from", str, required=True)
 @lightbulb.command("currency", "Convert value from one currency to another", aliases=["curr"], auto_defer=True)
 @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
 @filament.utils.pass_options
-async def currency(ctx: lightbulb.Context, origin, to , value) -> None:
-    
-    head = {
-        "Authorization": ksoft_key
-    }
-    params = {
+async def currency(ctx: lightbulb.Context, origin: str, to: str, val: int):
+    parameters = {
+        "api_key": curr_key,
         "from": origin,
         "to": to,
-        "value": value
+        "amount": val,
     }
-    async with ctx.bot.d.aio_session.get('https://api.ksoft.si/kumo/currency', headers=head, params=params) as resp:
+    async with ctx.bot.d.aio_session.get('https://api.getgeoapi.com/v2/currency/convert', params=parameters) as resp:
         data = json.loads(await resp.read())
-    try:
-        prt = data["pretty"]
-    except KeyError:
-        code = data["code"]
-        msg = data["message"]
-        raise ValueError(f"⚠ An Error Occured! **{msg.capitalize()}** (Code: {code})")
+
+
+    if data["status"] == "success":
+        origin_name = data["base_currency_name"]
+        origin_amount = data["amount"]
+        to_name = data["rates"][to.upper()]["currency_name"]
+        to_amount = data["rates"][to.upper()]["rate_for_amount"]
+    elif data["status"] == "failed":
+        err = data["error"]["message"]
+        code = data["error"]["code"]
+        return await ctx.respond(f"Error! {err} ({code})")
+    else:
+        await ctx.respond("Unable to parse the data! something is wrong...")
 
     emb = hikari.Embed(timestamp=datetime.now().astimezone())
     emb.add_field(
-        name=f"Conversion from {origin.upper()} to {to.upper()}", value=prt)
-    emb.set_footer("Data provided by: KSoft.Si")
+        name=f"Conversion from {origin_amount} {origin_name} to {to_name}", value=f"{to_amount} {to_name}"
+        )
     await ctx.respond(embed=emb)
 
 def load(bot):
