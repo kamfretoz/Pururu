fe9fd9113db5fed850605a7b67c7a16098f93495
KamFretoZ <iiibrahim89@gmail.com>
2022-04-07 18:59:17 +0700

    Fix typo

--- extensions/moderation/nickname.py
+++ extensions/moderation/nickname.py
@@ -1,13 +1,13 @@
 import lightbulb
 import hikari
 
-slowmode_plugin = lightbulb.Plugin("slowmode", "*talks in slow motion*")
-slowmode_plugin.add_checks(
+nickname_plugin = lightbulb.Plugin("nickname", "*Who are you again?*")
+nickname_plugin.add_checks(
     lightbulb.checks.has_guild_permissions(hikari.Permissions.CHANGE_NICKNAME),
     lightbulb.checks.bot_has_guild_permissions(hikari.Permissions.CHANGE_NICKNAME)
     )
 
-@slowmode_plugin.command()
+@nickname_plugin.command()
 @lightbulb.add_cooldown(3, 3, lightbulb.UserBucket)
 @lightbulb.option("nick", "the new nickname you want to set (leave empty to reset!)", str, required=False)
 @lightbulb.option("member", "The member you want to change the nickname", hikari.Member, required=True)
@@ -18,7 +18,7 @@ async def nickname(ctx: lightbulb.Context, member: hikari.Member, nick: str):
     await ctx.respond(f"Changed {member.mention} nickname to {nick}!")
     
 def load(bot):
-    bot.add_plugin(slowmode_plugin)
+    bot.add_plugin(nickname_plugin)
 
 def unload(bot):
-    bot.remove_plugin(slowmode_plugin)
+    bot.remove_plugin(nickname_plugin)
