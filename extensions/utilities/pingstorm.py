14ea55877660d63c8c660095147e5561e58772e4
KamFretoZ <iiibrahim89@gmail.com>
2022-04-10 18:12:56 +0700

    Fix cooldown

--- extensions/utilities/pingstorm.py
+++ extensions/utilities/pingstorm.py
@@ -1,12 +1,12 @@
 import lightbulb
 import hikari
 import asyncio
-from lightbulb.buckets import UserBucket, GuildBucket
+from lightbulb.buckets import GuildBucket
 
 pingstorm_plugin = lightbulb.Plugin("pingstorm", "ping deez nuts", include_datastore = True)
 
 @pingstorm_plugin.command()
-@lightbulb.add_cooldown(3600, 2, UserBucket)
+@lightbulb.add_cooldown(3600, 1, GuildBucket)
 @lightbulb.set_max_concurrency(1, GuildBucket)
 @lightbulb.option("amount", "The amount of the pings", int, required = False, default=5, min_value=1, max_value=100)
 @lightbulb.option("user", "The target user", hikari.Member, required = True)
@@ -17,7 +17,7 @@ async def pingstorm(ctx: lightbulb.Context, amount: int, user: hikari.Member) ->
         await ctx.respond("**WARNING:** **Maximum allowed amount is 100.**")
         return
     if user.id == ctx.bot.application.id:
-        await ctx.respond("HA! You think it'll work against me?? Nice Try.")
+        await ctx.respond("HA! You think it'll work against me?? Nice Try.", delete_after=3)
         user = ctx.author
         await asyncio.sleep(2)
     await ctx.respond("Ping Machine Initializing in 3 seconds!", delete_after=5)                
