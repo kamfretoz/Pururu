4842f0e2805bc4426c4efaa97a3167406256ee9c
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2023-02-05 22:58:24 +0700

    Update makeitquote.py

--- extensions/fun/makeitquote.py
+++ extensions/fun/makeitquote.py
@@ -39,26 +39,10 @@ aquote_plugin = lightbulb.Plugin("makequote", "Say wha?", include_datastore=True
 @lightbulb.command("Make Quote!", "Create a quote from someone's message", auto_defer = True)
 @lightbulb.implements(lightbulb.MessageCommand)
 async def makequote(ctx: lightbulb.Context) -> None:
-    if isinstance(ctx, lightbulb.PrefixContext):
-        if ctx.event.message.referenced_message is None:
-            await ctx.respond("❌ You haven't picked any message to create a quote from. **Please pick a message by replying to them whilst running this command**.")
-            return
-
-    
-    if isinstance(ctx, lightbulb.PrefixContext):
-        reply = ctx.event.message.referenced_message
-        author = reply.author
-
-        user = ctx.bot.cache.get_member(ctx.get_guild(), author)
-        
-        content = reply.content 
-    else:
-        message = ctx.options.target
-        author = message.author
-        
-        user = ctx.bot.cache.get_member(ctx.get_guild(), author)
-        
-        content = message.content
+    message = ctx.options.target
+    author = message.author
+    user = ctx.bot.cache.get_member(ctx.get_guild(), author)       
+    content = message.content
 
 
     name = user.username
