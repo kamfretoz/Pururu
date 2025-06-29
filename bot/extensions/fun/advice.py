ffa2afff251047c7c886827483f2453576e4c8cb
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-06-29 20:22:27 +0700

    fix oopsies

--- bot/extensions/fun/advice.py
+++ bot/extensions/fun/advice.py
@@ -1,11 +1,22 @@
 import lightbulb
+import aiohttp
+import hikari
+import json
 
 loader = lightbulb.Loader()
 
+
 @loader.command
-class Advice(lightbulb.SlashCommand, name="advice", description="Some advices that might aid you in your journey of life! :)"):
-    text: str = lightbulb.string("text", "The text to repeat")
+class Advice(
+    lightbulb.SlashCommand,
+    name="advice",
+    description="Some advices that might aid you in your journey of life! :)",
+):
 
     @lightbulb.invoke
-    async def invoke(self, ctx: lightbulb.Context) -> None:
-        await ctx.respond(self.text)
+    async def invoke(self, ctx: lightbulb.Context, cs: aiohttp.ClientSession) -> None:
+        async with cs.get("https://api.adviceslip.com/advice") as resp:
+            data = json.loads(await resp.read())
+        adv = data["slip"]["advice"]
+        emb = hikari.Embed(title="Here's some advice for you :)", description=adv)
+        await ctx.respond(embed=emb)
--- bot/extensions/utilities/echo.py
+++ bot/extensions/utilities/echo.py
@@ -1,18 +1,12 @@
 import lightbulb
-import hikari
-import aiohttp
-import json
 
 loader = lightbulb.Loader()
 
 
 @loader.command
 class Echo(lightbulb.SlashCommand, name="echo", description="Repeats the given text"):
+    text: str = lightbulb.string("text", "The text to repeat")
 
     @lightbulb.invoke
-    async def invoke(self, ctx: lightbulb.Context, cs: aiohttp.ClientSession) -> None:
-        async with cs.get('https://api.adviceslip.com/advice') as resp:
-            data = json.loads(await resp.read())
-        adv = data["slip"]["advice"]
-        emb = hikari.Embed(title="Here's some advice for you :)", description=adv)
-        await ctx.respond(embed=emb)
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        await ctx.respond(self.text)
