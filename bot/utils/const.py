77b5e5aabaab4cb7d295667040400692cd7beed3
KamFretoZ <14798312+kamfretoz@users.noreply.github.com>
2025-06-29 21:35:19 +0700

    add more server

--- bot/utils/const.py
+++ bot/utils/const.py
@@ -7,7 +7,8 @@ from hikari.api.config import CacheComponents
 
 GUILDS = [
         875986914367385600,
-        570976409452019722
+        570976409452019722,
+        793239269723471902 
 ]
 
 CACHE = CacheSettings(components=
